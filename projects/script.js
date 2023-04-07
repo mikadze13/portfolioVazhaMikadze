a580d7446083f52cbfb3fd54b2d564dfef8cc26c
Vazha Mikadze <74679350+mikadze13@users.noreply.github.com>
2023-04-07 16:14:08 +0400

    first commit

---
+++ projects/script.js
@@ -0,0 +1,166 @@
+$(document).ready(function () {
+
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function () {
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if (window.scrollY > 60) {
+            document.querySelector('#scroll-top').classList.add('active');
+        } else {
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+    });
+});
+
+document.addEventListener('visibilitychange',
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Projects | Portfolio Vazha Mikadze";
+            $("#favicon").attr("href", "/assets/images/favicon.png");
+        } 
+    });
+
+
+// fetch projects start
+function getProjects() {
+    return fetch("projects.json")
+        .then(response => response.json())
+        .then(data => {
+            return data
+        });
+}
+
+
+function showProjects(projects) {
+    let projectsContainer = document.querySelector(".work .box-container");
+    let projectsHTML = "";
+    projects.forEach(project => {
+        projectsHTML += `
+        <div class="grid-item ${project.category}">
+        <div class="box " style="width: 380px; margin: 1rem">
+      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <div class="content">
+        <div class="tag">
+        <h3>${project.name}</h3>
+        </div>
+        <div class="desc">
+          <p>${project.desc}</p>
+          <div class="btns">
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>`
+    });
+    projectsContainer.innerHTML = projectsHTML;
+
+    // vanilla tilt.js
+    // VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    //     max: 20,
+    // });
+    // // vanilla tilt.js  
+
+    // /* ===== SCROLL REVEAL ANIMATION ===== */
+    // const srtop = ScrollReveal({
+    //     origin: 'bottom',
+    //     distance: '80px',
+    //     duration: 1000,
+    //     reset: true
+    // });
+
+    // /* SCROLL PROJECTS */
+    // srtop.reveal('.work .box', { interval: 200 });
+
+    // isotope filter products
+    var $grid = $('.box-container').isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows',
+        masonry: {
+            columnWidth: 200
+        }
+    });
+
+    // filter items on button click
+    $('.button-group').on('click', 'button', function () {
+        $('.button-group').find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+}
+
+getProjects().then(data => {
+    showProjects(data);
+})
+// fetch projects end
+
+// Start of Tawk.to Live Chat
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
+// End of Tawk.to Live Chat
+
+// disable developer mode
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+
+// send information in firebase
+
+const db = firebase.firestore();
+
+const registrationsCollection = db.collection('registrations');
+
+function handleRegistrationSubmit(event){
+    event.preventDefault();
+
+
+    const formData = new FormData(event.target);
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const number = formData.get('number');
+    const message = formData.get('message');
+    
+
+registrationsCollection.add({
+    name,
+    email,
+    number,
+    message
+}).then(()=>{
+    console.log('registration information added to firestore!');
+
+}).catch(error =>{
+    console.log('error adding registration information to firestore:', error)
+}) ;
+}
+
+const registrationForm = document.getElementById('contact-form');
+registrationForm.addEventListener('submit', handleRegistrationSubmit);
